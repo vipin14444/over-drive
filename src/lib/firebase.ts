58ab6185f9d3ff50c5c87ddfe634bf1f9332f97a
Vipin Sharma <vipin.sharma3039@gmail.com>
2025-05-17 18:59:13 +0530

    Removed unnecessary assertions.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 import { env } from "~/env";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -9,13 +8,13 @@ import { env } from "~/env";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: env.NEXT_PUBLIC_FIREBASE_API_KEY as string,
-  authDomain: env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN as string,
-  projectId: env.NEXT_PUBLIC_FIREBASE_PROJECT_ID as string,
-  storageBucket: env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET as string,
-  messagingSenderId: env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID as string,
-  appId: env.NEXT_PUBLIC_FIREBASE_APP_ID as string,
-  measurementId: env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID as string,
+  apiKey: env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
